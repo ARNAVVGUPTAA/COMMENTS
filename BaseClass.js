09059c27ded8f0bc6ee21edf26fe72aeaa06b2ab
ARNAVVGUPTA <gupta.arun47@gmail.com>
2020-04-28 18:03:38 +0530

    COMMENTED

--- BaseClass.js
+++ BaseClass.js
@@ -1,22 +1,30 @@
+//creating a class which would act as a parent for other classes and would define the properties of the ground 
 class BaseClass{
     constructor(x, y, width, height, angle) {
         var options = {
+          //properties of the class
             'restitution':0.8,
             'friction':1.0,
             'density':1.0
         }
+        //this would make a body in the shape of the rectangle and the properties given above
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
+        //loads the image to use
         this.image = loadImage("sprites/base.png");
+        //adds the body to the our world
         World.add(world, this.body);
       }
       display(){
+        //namespacing time!!
         var angle = this.body.angle;
+        //makes the body rotate like in the real world
         push();
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
         imageMode(CENTER);
+        //the most important part; helps in displaying the image which the following properties
         image(this.image, 0, 0, this.width, this.height);
         pop();
       }
--- sketch.js
+++ sketch.js
@@ -1,23 +1,29 @@
+//making the names of the Engine, the World, the Bodies and the constraining between the two bodies smaller
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
+//defining the variables to be used
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var bird, slingShot;
 
 function preload() {
+    //loading the image for background
     backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
+    //creating the canvas
     var canvas = createCanvas(1200,400);
+    //creating our physics engine which is to be used
     engine = Engine.create();
+    //adding our world to the engine we've just created
     world = engine.world;
 
-
+    //creating the objects such ass ground, platform, the boxes, the pigs, the logs and the bird
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -38,14 +44,18 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+    //creating the slingshot
+    slingShot = new Sling(bird.body,{x:200,y:100});
 }
 
 function draw(){
+    //changing the background to the image we've loaded
     background(backgroundImg);
+    //it keeps updating the engine which would help is making the physics engine work
     Engine.update(engine);
     strokeWeight(4);
+    //calling the function display for every object we've created
     box1.display();
     box2.display();
     ground.display();
@@ -63,6 +73,14 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    //log6.display();
+    slingShot.display();    
+}
+function mouseDragged(){
+    //whenever the mouse is dragged, the birds position should change to that of the mouse's
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+    //whenever the mouse is dragged and then released, the bird should fly!!!
+    slingShot.fly();
 }
